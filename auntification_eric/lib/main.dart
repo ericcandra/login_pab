c9807af8e527c79cd174657e8ab79842b82204ef
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-11-30 10:42:02 +0700

    login pab

---
+++ auntification_eric/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  runApp(MainApp(isLoggedIn: isLoggedIn,));
+}
+
+class MainApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MainApp({super.key, required this.isLoggedIn});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
